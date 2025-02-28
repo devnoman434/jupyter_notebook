f37dc2649b8bb083f1a842abd3a52eef3da5ad72
devnoman434 <noman@galixo.ai>
2025-02-28 17:55:41 +0500

    jupyter notebook analysis on different things

---
+++ .ipynb_checkpoints/script-checkpoint.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+sugar = pd.read_csv('diabetes.csv')
+print(sugar.head())
---
+++ script.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+air = pd.read_csv('Air Conditioners.csv')
+print(air.head())
+
